1137bbe23e62688fe30504ce1cb8d7fc01e04bb7
Marwan Rahim <marwan820@Marwan>
2023-09-11 14:05:22 -0500

    complete lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,33 @@
 import React from "react";
 import { actors } from "../data";
+import { v4 as uuidv4 } from "uuid";
+
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  console.log("Actors", actors);
+
+  const actorsList = actors.map(({ name, movies }) => {
+    return (
+      <div key={uuidv4()}>
+        <p>{name}</p>
+        <ul>
+          {Object.entries(movies).map((movie) => {
+            return(
+            <li key={uuidv4()}>{movie[1]}</li>);
+  })}
+        </ul>
+      </div>
+    );
+  });
+
+  console.log(actorsList)
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorsList}
+    </div>
+  );
 }
 
 export default Actors;
