a4d871231977d8af8198e528c5ce228a7adde109
Marwan Rahim <marwan820@Marwan>
2023-09-11 13:48:55 -0500

    passed Directors test

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { directors } from "../data";
+import { v4 as uuidv4 } from "uuid";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  console.log("Directors:", directors);
+
+  const directorList = directors.map(({ name, movies }) => {
+    return (
+      <div key={uuidv4()}>
+        <p>{name}</p>
+        <ul>
+          {Object.entries(movies).map((movie) => (
+            <li key={uuidv4()}>{movie[1]}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorList}
+    </div>
+  );
 }
 
 export default Directors;
