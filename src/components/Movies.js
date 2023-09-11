ded40114f27e83e656d04eb97c6ebbba57dcdca7
Marwan Rahim <marwan820@Marwan>
2023-09-11 13:36:48 -0500

    passed movies test

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { movies } from "../data";
+import { getByTitle } from "@testing-library/react";
+import { v4 as uuidv4} from "uuid";
+
+
+
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+ console.log(movies)
+
+
+  const moviesList = movies.map(({title,time,genres}) => {
+    return(
+
+
+      <div key={uuidv4()}>
+        <p>{title}</p>
+        <p>{time}</p>
+        <ul>
+         {Object.entries(genres).map(genre =>  <li key={uuidv4()}>{genre[1]}</li>)}
+        </ul>
+      </div> 
+
+
+    )})
+
+
+  return <div>
+    <h1>Movies Page</h1>
+    {moviesList}
+  </div>;
 }
 
 export default Movies;
